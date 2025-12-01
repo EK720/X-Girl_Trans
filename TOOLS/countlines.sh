af78e1dd1fd995e4a1a3a4f3bbacac342a62c7ec
OldCorpo <oldcorponet@outlook.com>
2025-12-01 14:03:28 +0100

    Updating the script to track the advancement of the translation.

--- TOOLS/countlines.sh
+++ TOOLS/countlines.sh
@@ -6,7 +6,7 @@ echo "Total number of lines"
 grep -E '\(str ".{3,}"|\(text ".{3,}"' ../MES/*.rkt | grep -v '「' | wc -l
 
 echo "Translated lines"
-grep -E '\(str ".{3,}"|\(text ".{3,}"' ../MES/*.rkt | grep -v '「' | grep -E '"[A-Za-z0-9]|"[. ]+[A-Za-z0-9.-]' | wc -l
+grep -E '\(str ".{3,}"|\(text ".{3,}"' ../MES/*.rkt | grep -v '「' | grep -E '"[A-Za-z0-9]|"[. ]+[A-Za-z0-9.-]|\*|Exit|Click|−ｔｈｅｒｅ−|music room' | wc -l
 
 echo "Untranslated lines"
-grep -E '\(str ".{3,}"|\(text ".{3,}"' ../MES/*.rkt | grep -v '「' | grep -v -E '"[A-Za-z0-9]|"[. ]+[A-Za-z0-9.-]' | wc -l
+grep -E '\(str ".{3,}"|\(text ".{3,}"' ../MES/*.rkt | grep -v '「' | grep -v -E '"[A-Za-z0-9]|"[. ]+[A-Za-z0-9.-]|\*|Exit|Click|−ｔｈｅｒｅ−|music room' | wc -l
